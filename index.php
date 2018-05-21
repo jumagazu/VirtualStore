db50e80c3fc16ac452f4ab39256a57aa616fe3a0
jumagazu <juan12-95@hotmail.com>
2018-05-21 12:37:24 -0500

    prueba del versionador

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-// el archivo autoload inicializa todos lo archivos necesarios para que el framework funcione
+// El archivo autoload inicializa todos lo archivos necesarios para que el framework funcione
 include "core/autoload.php";
 
 
-// cargamos el modulo iniciar.
+// Cargamos el modulo iniciar.
 $lb = new Lb();
 $lb->loadModule("index");
 
