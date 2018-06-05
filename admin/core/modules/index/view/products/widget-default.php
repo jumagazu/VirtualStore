c82cc50b4044897e528fa41885e8e13261e1e70f
jumagazu <juan12-95@hotmail.com>
2018-06-05 00:32:25 -0500

    se implementa modal de confirmación para eliminación de productos

--- admin/core/modules/index/view/products/widget-default.php
+++ admin/core/modules/index/view/products/widget-default.php
@@ -39,9 +39,32 @@ $categories = ProductData::getAll();
                         <td style="width:105px;">
                         <a " title="Ir a Tienda"href="../index.php?view=producto&product_id=<?php echo $cat->id; ?>" target="_blank" class="btn btn-default btn-xs"><i class="fa fa-link"></i></a> 
                         <a " title="Actualizar producto"href="index.php?view=editproduct&product_id=<?php echo $cat->id; ?>" class="btn btn-warning btn-xs"><i class="fa fa-edit"></i></a> 
-                        <a " title="Eliminar producto"href="index.php?action=delproduct&product_id=<?php echo $cat->id; ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a> 
+                        <a " title="Eliminar producto" data-toggle="modal" data-target="#modal<?php echo $cat->id; ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a> 
                         </td>
                         </tr>
+
+
+                         <!-- Modal -->
+                        <div class="modal fade" id="modal<?php echo $cat->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Borrar producto</h5>
+                                    </div>
+                                    <div class="modal-body">
+                                        &iquest;Est&#225;s seguro que deseas borrar el producto "<?php echo $cat->name;?>"&#63;,<br>
+                                        de ser as&#237; no podr&#225;s recuperar el producto...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                        <a title="Eliminar producto" href="index.php?action=delproduct&product_id=<?php echo $cat->id;?>" class="btn btn-danger">Borrar producto</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
 <?php endforeach; ?>
                       </tbody>
                     </table>
